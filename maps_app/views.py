24c5fb63fd05bb12a07cd80650dacd7ac943a270
Taha Ali <shaikhtahaali02@gmail.com>
2026-07-06 18:07:30 +0530

    interact with leaflet.js

--- maps_app/views.py
+++ maps_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    data = {
+        "latitude": 24.38,
+        "longitude": 46.43,
+        "zoom": 5,
+    }
+    return render(request,'maps_app/home.html',{'data':data})
